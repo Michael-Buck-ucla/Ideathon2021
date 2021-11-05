1e545dcc8042068e09ee868b92464d309ee83cde
Daniel Ferguson <danferg1@hotmail.com>
2021-11-05 11:49:33 -0700

    added eveent model

--- ideathonwebsite/ideathonwebapp/models.py
+++ ideathonwebsite/ideathonwebapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+	AGE_RANGES = (
+        ('AA', 'All Ages'),
+        ('18', '18+'),
+        ('21', '21+'),
+    )
+	name = models.CharField(max_length=255)
+	address = models.SlugField(unique=True, max_length=255) #need address
+	genre = models.CharField(max_length=255)
+	startTime = models.DateTimeField(auto_now_add=True)
+	endTime = models.DateTimeField(auto_now_add=True)
+	host = models.CharField(max_length=255)
+	audienceNumber = models.IntegerField()
+	entryCost = models.FloatField()
+	ageRange = models.CharField(max_length=2, choices=AGE_RANGES)
+
+
