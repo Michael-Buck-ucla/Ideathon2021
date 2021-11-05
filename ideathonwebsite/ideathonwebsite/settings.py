a4c8f5fb9cd369e7eaa52bc6305c35be6abe6c99
Michael Buck <michaelbuck14@ucla.edu>
2021-11-05 16:21:10 -0700

    Basic map

--- ideathonwebsite/ideathonwebsite/settings.py
+++ ideathonwebsite/ideathonwebsite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ideathonwebapp'
 ]
 
 MIDDLEWARE = [
@@ -121,9 +120,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-GOOGLE_API_KEY = "XXX"
+GOOGLE_API_KEY = "AIzaSyBycYunkc2ygCKEQy0UQKkOjuaWZYQHyFQ"
