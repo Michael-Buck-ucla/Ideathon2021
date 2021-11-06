a3e6dcf599611b190b0ecf57c12fb947d1ba9f45
Michael Buck <michaelbuck14@ucla.edu>
2021-11-05 18:53:32 -0700

    fixed

--- ideathonwebsite/ideathonwebapp/static/js/index.js
+++ ideathonwebsite/ideathonwebapp/static/js/index.js
@@ -5,20 +5,11 @@ function initMap() {
     center: { lat: 34, lng: -118 },
     zoom: 4,
   });
-  // console.log("hello1");
-  console.log(locations);
-  // console.log("hello2");
+
   for (var i in locations) {
     marker = new google.maps.Marker({
       position: { lat: locations[i]["lat"], lng: locations[i]["long"] },
       map: map,
     });
   }
-}
-
-function createMarker(latitude, longitude) {
-  marker = new google.maps.Marker({
-    position: { lat: latitude, lng: longitude },
-    map: map,
-  });
 }
--- ideathonwebsite/ideathonwebapp/views.py
+++ ideathonwebsite/ideathonwebapp/views.py
@@ -7,11 +7,12 @@ from .models import Event
 # Create your views here.
 def home(request):
     event_list = Event.objects.all()
-    locations = []
-    for event in event_list:
-        locations.append({'lat':event.latitude, 'long':event.longitude})
+    # locations = []
+    # for event in event_list:
+    #     locations.append({'lat':event.latitude, 'long':event.longitude})
     context = {}
     context['api_key'] = settings.GOOGLE_API_KEY
-    # context['locations'] = json.dumps([{'lat' : 32, 'long' : 120}, {'lat' : 29, 'long' : 120} ])
-    context['locations'] = json.dumps( locations )
+    locations = [{'lat' : 32, 'long' : 120}, {'lat' : 29, 'long' : 120}, {'lat' : 34, 'long' : 100}]
+    context['locations'] = json.dumps(locations)
+    # json.dumps( [{"lat" : 32, "long" : 120}, {"lat" : 29, "long" : 120}])
     return render(request,'home.html', context)
